b3c5917e655f41066d028201e56ab2ded83a29e6
Apetrei <myrel1987@gmail.com>
2021-11-16 18:08:11 +0100

    first commit of the code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Axios from 'axios'
+import './App.css';
+
+
+const App = () => {
+   const APP_ID = "02752619";
+
+   const APP_KEY = 
+   "a6ecbf0852f0fb833efb37522564ce1f";
+
+   const url = 
+   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+     const getData = async() => {
+      const result = await Axios.get(url);
+
+      console.log(result)
+     }
+   return (
+      <div>
+         <h1>Food Searching App</h1>
+      </div>
+   );
+};
+
+export default App
